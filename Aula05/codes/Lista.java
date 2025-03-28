116bca5a01775403b96f868b9d5bc5dd8c2e8034
lucianoferretto <prof.ferretto@yahoo.com.br>
2025-03-28 15:59:07 -0300

    Apresentação e códigos Aula 05

---
+++ Aula05/codes/Lista.java
@@ -0,0 +1,16 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class Lista {
+    public static void main(String[] args) {
+        //Lista Imutável usando o List.of()
+        List<String> listaImutavel = List.of("A", "B", "C");
+
+        //Lista de tamanho fixo usando o Arrays.asList()
+        List<String> listaTamanhoFixo = Arrays.asList("X", "Y", "Z");
+        
+        //Testando operações de adição e remoção
+        listaImutavel.add("D");
+        listaTamanhoFixo.remove("X");
+    }
+}
